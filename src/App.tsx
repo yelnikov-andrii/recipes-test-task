83febcc32c99eb4466b2b1e73655289b3e00b870
Andrii <whiteyelnikov@gmail.com>
2025-03-10 15:45:12 +0200

    added header and categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './components/main/Main';
+import Header from './components/templates/Header';
+import { fetchCategories } from './app/action-creator/categories/fetchCategories';
 
 function App() {
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route element={<Main />} path="/">
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
